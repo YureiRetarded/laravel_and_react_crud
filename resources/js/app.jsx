841f20be1c2ee85ab3c540f95d2a5fda754c225c
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2023-01-26 17:32:28 +0300

    feature/migrate_project_to_linux 	I swithed main os to linux

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import './bootstrap';
+import React from "react";
+import ReactDom from 'react-dom/client'
+import App from './components/App'
+
+ReactDom.createRoot(document.getElementById('app')).render(
+    <App/>
+)
